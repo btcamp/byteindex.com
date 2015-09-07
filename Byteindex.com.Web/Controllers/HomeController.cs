68df49cd27582ff7e4a588604c6f773b25255913
simple.jiang <jiangchun1320@hotmail.com>
2015-09-07 16:19:15 +0800

    modify

---
+++ Byteindex.com.Web/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Byteindex.com.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            string path = Server.MapPath("~/log/newslog.txt");
+            if (System.IO.File.Exists(path))
+            {
+                string[] array = System.IO.File.ReadAllLines(path);
+                List<Models.NewsViewModel> result = ConverToModel(array);
+            }
+            //System.IO.File.ReadAllLines()
+            return View();
+        }
+
+        public List<Models.NewsViewModel> ConverToModel(string[] array)
+        {
+            List<Models.NewsViewModel> list = new List<Models.NewsViewModel>();
+            foreach (string item in array)
+            {
+                Models.NewsViewModel model = new Models.NewsViewModel();
+                string[] info = item.Split(new string[] { "^_^" }, StringSplitOptions.RemoveEmptyEntries);
+                if (info.Length == 2)
+                {
+                    model.Time = DateTime.ParseExact(info[0], "yyyyMMddHHmmss", CultureInfo.CurrentCulture);
+                    model.Path = string.Format("/news/{0}.html", info[0]);
+                    model.Title = info[1];
+                    list.Add(model);
+                }
+            }
+            return list.OrderByDescending(e => e.Time).Take(3).ToList();
+        }
+    }
+}
---
+++ Byteindex.com.Web/Models/NewsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Byteindex.com.Web.Models
+{
+    public class NewsViewModel
+    {
+        public DateTime Time { get; set; }
+        public string Title { get; set; }
+        public string Path { get; set; }
+    }
+}
